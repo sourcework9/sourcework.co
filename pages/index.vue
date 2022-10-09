fef1a2a23896a71fa4e6c2f7f8791776d6f69ba6
Gokul <gokulsai.alla@gmail.com>
2022-10-09 13:56:56 -0700

    Nuxtjs boilerplate tailwindcss pug

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.container
+    div
+        h1.title
+        | pug-starter sasdfasdf
+        .links
+        a.button--green(href='https://nuxtjs.org/' target='_blank' rel='noopener noreferrer')
+            | Documentation
+        a.button--grey(href='https://github.com/nuxt/nuxt.js' target='_blank' rel='noopener noreferrer')
+            | GitHub
+</template>
