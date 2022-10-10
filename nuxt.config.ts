32b5f1bac1916f8486cb1909b4afda920da6e5ef
SW <sourcework.co@gmail.com>
2022-10-10 08:35:42 -0700

    CI/CD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,36 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
-    router: {
-        base: '/',
-        extendRoutes(routes) {
-            for (const key in routes) {
-                routes[key].caseSensitive = true
-            }
-        }
+  target: "static",
+  router: {
+    base: "/",
+    extendRoutes(routes) {
+      for (const key in routes) {
+        routes[key].caseSensitive = true;
+      }
     },
+  },
 
-    css: [
-        '@/assets/css/tailwind.css',
+  css: ["@/assets/css/tailwind.css"],
+
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
+    },
+  },
+
+  head: {
+    title: "SourceWork - Hire remote full-time developers from India",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description" },
+      { link: { rel: "stylesheet", href: "/css/tailwind.css" } },
     ],
-
-    build: {
-        postcss: {
-          postcssOptions: require('./postcss.config.js'),
-        },
-      },
-
-    head: {
-        title: "SourceWork - Hire remote full-time developers from India",
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description' },
-           { link: { rel: 'stylesheet', href: '/css/tailwind.css' }}
-        ],
-        link: [
-            { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' },
-            { rel: 'icon', type: "image/png", sizes: "32x32", href: '/favicon-32x32.png' },
-            { rel: 'icon', type: "image/png", sizes: "16x16", href: '/favicon-16x16.png' },
-            { rel: 'manifest', href: '/site.webmanifest' },
-        ],
-    },
-})
+    link: [
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
+    ],
+  },
+});
